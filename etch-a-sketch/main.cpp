7838754b36b45b26e1ef13984c9917709a59fad6
Cole Zich <zich.cole@gmail.com>
2020-04-25 12:17:05 -0400

    j
    i need to update it i guess

--- etch-a-sketch/main.cpp
+++ etch-a-sketch/main.cpp
@@ -9,5 +9,48 @@
 #include <iostream>
 
 int main() {
-    s
+    const char up = 65;
+    const char down = 66;
+    const char left = 68;
+    const char right = 67;
+    char keyPressed;
+    
+    std::cout << "Press Key (up, down, left, right)\n";
+    std::cout << "_________________________________\n";
+    std::cout << "|                               |\n";
+    std::cout << "|                               |\n";
+    std::cout << "|                               |\n";
+    std::cout << "|                               |\n";
+    std::cout << "|                               |\n";
+    std::cout << "|                               |\n";
+    std::cout << "|                               |\n";
+    std::cout << "|                               |\n";
+    std::cout << "|                               |\n";
+    std::cout << "|                               |\n";
+    std::cout << "|                               |\n";
+    std::cout << "|_______________________________|\n";
+    
+    for (int i = 0; i < 500; i++) {
+        std::cin >> keyPressed;
+        
+        if (i == 499) {
+            std::cout << "_________________________________\n";
+            std::cout << "|                               |\n";
+            std::cout << "|                               |\n";
+            std::cout << "|                               |\n";
+            std::cout << "|                               |\n";
+            std::cout << "|                               |\n";
+            std::cout << "|                               |\n";
+            std::cout << "|                               |\n";
+            std::cout << "|                               |\n";
+            std::cout << "|                               |\n";
+            std::cout << "|                               |\n";
+            std::cout << "|                               |\n";
+            std::cout << "|_______________________________|\n";
+        } else if (up == keyPressed) {
+            std::cout << ".";
+        }
+        
+        
+    }
 }
